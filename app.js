c26af3bd5be9ba00bca3410b479283dc4dc557c1
Jasleen Sondhi <jasleensondhi@gmail.com>
2020-11-14 00:52:21 +0530

    Final commit

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const http = require('http');
-const port=process.env.PORT || 3000
+const http = require('http')
+const fs = require('fs')
+
 const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
-});
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(process.env.PORT || 3000)
